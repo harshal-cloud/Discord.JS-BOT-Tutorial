59fe3bc9e370743156ca5a2e37cd3cd9c82f68bc
Ubaid7 <75165174+Ubaid7@users.noreply.github.com>
2021-01-20 11:08:58 +0300

    Delete youtube.js

--- Episode 5/commands/youtube.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    commands: ['youtube', 'yt'],
-    description: 'Sends YT Link', //Optional
-    callback: (message, args) => {
-        message.channel.send('https://youtube.com/techtipcyber')
-    }
-}
